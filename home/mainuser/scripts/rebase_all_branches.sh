7d22852345d98f193c8d18f957291c46a12ff489
Jacek Kuczera <jacek.kuczera@gmail.com>
2019-08-31 23:29:27 +0200

    add rebase_all_branches script

---
+++ home/mainuser/scripts/rebase_all_branches.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+CURRENT_COMMIT=$(git --git-dir=$HOME/.mydotfiles/ --work-tree=/ status | awk 'NR==1 { print $NF; exit }')
+
+if [ -z $CURRENT_COMMIT ]; then
+    echo "cannot get the current commit!"
+    exit 1
+fi
+
+echo "saving current commit - $CURRENT_COMMIT"
+
+echo "fetching origin..."
+git --git-dir=$HOME/.mydotfiles/ --work-tree=/ fetch origin
+
+if [ $? -ne 0 ]; then
+    echo "cannot fetch origin!"
+    exit 1
+fi
+
+echo "checking out home..."
+git --git-dir=$HOME/.mydotfiles/ --work-tree=/ checkout home
+
+if [ $? -ne 0 ]; then
+    echo "cannot checkout home!"
+    exit 1
+fi
+
+git --git-dir=$HOME/.mydotfiles/ --work-tree=/ rebase master
+
+if [ $? -ne 0 ]; then
+    echo "cannot rebase home!"
+    git --git-dir=$HOME/.mydotfiles/ --work-tree=/ rebase --abort
+    #exit 1  # we do not exit at this point, as we want to go back to $CURRENT_COMMIT
+fi
+
+echo "returning to $CURRENT_COMMIT..."
+git --git-dir=$HOME/.mydotfiles/ --work-tree=/ checkout $CURRENT_COMMIT
+
+if [ $? -ne 0 ]; then
+    echo "cannot return to $CURRENT_COMMIT"
+    exit 1
+fi
+
+echo "done"
