d579e7a0bae76707e881718aa3388b167a2cef0a
Jacek Kuczera <jacek.kuczera@gmail.com>
2020-05-10 22:48:12 +0200

    use sudo in rebase_home.sh

--- home/mainuser/scripts/rebase_home.sh
+++ home/mainuser/scripts/rebase_home.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-CURRENT_COMMIT=$(git --git-dir=$HOME/.mydotfiles/ --work-tree=/ status | awk 'NR==1 { print $NF; exit }')
+CURRENT_COMMIT=$(sudo -E git --git-dir=$HOME/.mydotfiles/ --work-tree=/ status | awk 'NR==1 { print $NF; exit }')
 
 if [ -z $CURRENT_COMMIT ]; then
     echo "cannot get the current commit!"
@@ -10,7 +10,7 @@ fi
 echo "saving current commit - $CURRENT_COMMIT"
 
 echo "fetching origin..."
-git --git-dir=$HOME/.mydotfiles/ --work-tree=/ fetch origin
+sudo -E git --git-dir=$HOME/.mydotfiles/ --work-tree=/ fetch origin
 
 if [ $? -ne 0 ]; then
     echo "cannot fetch origin!"
@@ -18,23 +18,23 @@ if [ $? -ne 0 ]; then
 fi
 
 echo "checking out home..."
-git --git-dir=$HOME/.mydotfiles/ --work-tree=/ checkout home
+sudo -E git --git-dir=$HOME/.mydotfiles/ --work-tree=/ checkout home
 
 if [ $? -ne 0 ]; then
     echo "cannot checkout home!"
     exit 1
 fi
 
-git --git-dir=$HOME/.mydotfiles/ --work-tree=/ rebase master
+sudo -E git --git-dir=$HOME/.mydotfiles/ --work-tree=/ rebase master
 
 if [ $? -ne 0 ]; then
     echo "cannot rebase home!"
-    git --git-dir=$HOME/.mydotfiles/ --work-tree=/ rebase --abort
+    sudo -E git --git-dir=$HOME/.mydotfiles/ --work-tree=/ rebase --abort
     #exit 1  # we do not exit at this point, as we want to go back to $CURRENT_COMMIT
 fi
 
 echo "returning to $CURRENT_COMMIT..."
-git --git-dir=$HOME/.mydotfiles/ --work-tree=/ checkout $CURRENT_COMMIT
+sudo -E git --git-dir=$HOME/.mydotfiles/ --work-tree=/ checkout $CURRENT_COMMIT
 
 if [ $? -ne 0 ]; then
     echo "cannot return to $CURRENT_COMMIT"
