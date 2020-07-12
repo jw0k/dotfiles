613689a158c138c9d59945da4368cfe918543e6d
Jacek Kuczera <jacek.kuczera@rst.com.pl>
2020-07-12 17:07:48 +0200

    modify power values for L460

--- home/mainuser/scripts/power_normal.sh
+++ home/mainuser/scripts/power_normal.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-sed -i 's/^power package.*/power package 40\/2 15/' /etc/intel-undervolt.conf
+sed -i 's/^power package.*/power package 40\/2 25/' /etc/intel-undervolt.conf
 pkill -USR1 intel-undervolt
 intel-undervolt apply
