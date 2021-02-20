a7f60f1cc48858e0ca31342e343be682f724110e
Jacek Kuczera <jacek.kuczera@gmail.com>
2021-02-20 18:59:39 +0100

    add fan-controller scripts

--- home/mainuser/scripts/i3blocks/cpu_temp.sh
+++ home/mainuser/scripts/i3blocks/cpu_temp.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 
+speed=0
+tries=0
+function changeSpeed() { # param: newSpeed
+    newSpeed="$1"
+    if (( newSpeed != speed )); then
+        (( tries++ ))
+        if (( tries == 2 )); then
+            tries=0
+            speed=$newSpeed
+        fi
+    else
+        tries=0
+    fi
+}
+
 first=true
 counter=0
 pkill -f "turbostat.*PkgTmp"
@@ -12,6 +27,24 @@ turbostat -q --interval 5 --show PkgTmp --cpu "" -S | while read line; do
         else
             counter=0
         fi
+
+        if (( line < 40 )); then
+            changeSpeed 0
+        elif (( line >= 40 && line < 50 )); then
+            changeSpeed 1
+        elif (( line >= 50 && line < 60 )); then
+            changeSpeed 32
+        elif (( line >= 60 && line < 70 )); then
+            changeSpeed 50
+        elif (( line >= 70 && line < 80 )); then
+            changeSpeed 80
+        elif (( line >= 80 )); then
+            changeSpeed 100
+        fi
+
+        speedPadded=`printf %03d $speed`
+
+        [ -c /dev/fan_controller_ch340 ] && echo -ne "s${speedPadded}" > /dev/fan_controller_ch340
 
         RESULT="🌡️"
         RESULT+=" ${line}°C"
---
+++ home/mainuser/scripts/set_fan_controller_baud_rate.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+stty -F /dev/fan_controller_ch340 9600 -hupcl
